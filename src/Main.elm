8c2a71255b1720927ee343c6024dea213b508cea
Ulric Wilfred <shaman.sir@gmail.com>
2019-02-25 22:55:37 +0100

    work on alternative way to construct render queue

--- src/Main.elm
+++ src/Main.elm
@@ -843,6 +843,67 @@ mapControls model controlsMsg =
         Controls.Rotate th -> Rotate th
 
 
+type RenderQueueItem = Empty | ToCanvas (Array WebGL.Entity) | ToHtml (Array (Html Msg))
+type alias RenderQueue = Array RenderQueueItem
+
+
+groupLayers : Model -> RenderQueue
+groupLayers model =
+    let
+        viewport = getViewportState model |> Viewport.find
+        addToQueue (index, layer) renderQueue =
+            let
+                indexOfThelastInQueue = Array.length renderQueue - 1
+                lastInQueue = renderQueue |> Array.get indexOfThelastInQueue
+            in case layer.layer of
+                WebGLLayer _ _ ->
+                    case lastInQueue of
+                        Nothing ->
+                            renderQueue
+                                |> Array.push
+                                    (layerToEntities model viewport index layer
+                                        |> Array.fromList
+                                        |> ToCanvas)
+                        Just (ToCanvas curEntities) ->
+                            renderQueue
+                                |> Array.set indexOfThelastInQueue
+                                    (layerToEntities model viewport index layer
+                                        |> Array.fromList
+                                        |> Array.append curEntities
+                                        |> ToCanvas)
+                        Just _ ->
+                            renderQueue
+                                |> Array.push
+                                    (layerToEntities model viewport index layer
+                                        |> Array.fromList
+                                        |> ToCanvas)
+                HtmlLayer _ _ ->
+                    case lastInQueue of
+                        Nothing ->
+                            renderQueue
+                                |> Array.push
+                                    (Array.empty
+                                        |> Array.push (layerToHtml model index layer)
+                                        |> ToHtml)
+                        Just (ToHtml curHtml) ->
+                            renderQueue
+                                |> Array.set indexOfThelastInQueue
+                                    (Array.empty
+                                        |> Array.push (layerToHtml model index layer)
+                                        |> Array.append curHtml
+                                        |> ToHtml)
+                        Just _ ->
+                            renderQueue
+                                |> Array.push
+                                    (Array.empty
+                                        |> Array.push (layerToHtml model index layer)
+                                        |> ToHtml)
+    in
+        model.layers
+            |> List.indexedMap Tuple.pair
+            |> List.foldl addToQueue Array.empty
+
+
 isWebGLLayer : Layer -> Bool
 isWebGLLayer layer =
     case layer of
