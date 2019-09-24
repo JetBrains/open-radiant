caf9b134c9eedbcad572a02b1cd8dd0d9f19dc34
Ulric Wilfred <shaman.sir@gmail.com>
2019-09-24 11:12:41 +0200

    create a draft of the separate `LayerDef` definition to convert layers through it instead of the current model

--- src/Model/LayerDef.elm
+++ src/Model/LayerDef.elm
@@ -3,6 +3,8 @@ module LayerDef exposing (..)
 import Html exposing (Html)
 import Html as H exposing (..)
 
+import Dict
+
 import Json.Decode as D
 import Json.Encode as E
 
@@ -14,13 +16,22 @@ type Kind
     | JS
 
 
+type alias CreateLayer model view msg =
+    (String -> Maybe (LayerDef model view msg))
+
+
+type alias LayerRegistry model view msg =
+    Dict.Dict String (LayerDef model view msg)
+
+
+
 type alias LayerDef model view msg =
     { id : String
     , kind : Kind
     , init : model
     , encode : model -> E.Value
     , decode : D.Decoder model
-    , update : msg -> model -> model
+    , update : msg -> model -> ( model, Cmd msg )
     , view : model -> Blend -> view
     }
 
@@ -28,6 +39,10 @@ type alias LayerDef model view msg =
 -- type alias Layers layer = List (Kind -> layer)
 
 type alias Blend = String
+
+type Visibility
+    = Visible
+    | Hidden
 
 type LayerModel
     = Background ()
@@ -39,19 +54,60 @@ type Msg
     | CoverMsg ()
 
 
-renderLayer : LayerModel -> Html Msg
-renderLayer layerModel =
-    case layerModel of
-        Background bgModel -> Background.view bgModel blend
+registry : LayerRegistry LayerModel (Html Msg) Msg
+registry = Dict.empty
 
 
-updateLayer : Msg -> LayerModel -> LayerModel
-updateLayer msg layerModel =
-    case ( msg, layerModel ) of
-        ( BackgroundMsg bgMsg, Background bgModel ) ->
-            Background.update bgMsg bgModel
+adapt
+     : (model -> LayerModel)
+    -> (msg -> Msg)
+    -> (Kind -> view -> Html Msg)
+    -> (LayerModel -> Maybe model)
+    -> (Msg -> Maybe msg)
+    -> LayerDef model view msg
+    -> LayerDef LayerModel (Html Msg) Msg
+adapt convertModel convertMsg convertView extractModel extractMsg source =
+    { id = source.id
+    , kind = source.kind
+    , init = convertModel source.init
+    , encode =
+        \layerModel ->
+            case extractModel layerModel of
+                Just m -> source.encode m
+                Nothing -> E.string <| "wrong encoder for " ++ source.id
+    , decode = source.decode |> D.map convertModel
+    , update =
+        \mainMsg layerModel ->
+            case ( extractMsg mainMsg, extractModel layerModel ) of
+                ( Just msg, Just model ) ->
+                    source.update msg model
+                        |> Tuple.mapFirst convertModel
+                        |> Tuple.mapSecond (Cmd.map convertMsg)
+                _ -> -- FIXME: return Maybe for the case when message / model doesn't match
+                    ( convertModel source.init
+                    , Cmd.none
+                    )
+    , view =
+        \layerModel blend ->
+            case extractModel layerModel of
+                Just model -> convertView source.kind <| source.view model blend
+                Nothing -> H.div [] []
+    }
 
 
-renderLayers : List LayerModel -> (LayerModel -> Html Msg) -> Html Msg
-renderLayers layerModels renderF =
-    H.div [] <| List.map renderF layerModels
+-- renderLayer : LayerModel -> Html Msg
+-- renderLayer layerModel =
+--     case layerModel of
+--         Background bgModel -> Background.view bgModel blend
+
+
+-- updateLayer : Msg -> LayerModel -> LayerModel
+-- updateLayer msg layerModel =
+--     case ( msg, layerModel ) of
+--         ( BackgroundMsg bgMsg, Background bgModel ) ->
+--             Background.update bgMsg bgModel
+
+
+-- renderLayers : List LayerModel -> (LayerModel -> Html Msg) -> Html Msg
+-- renderLayers layerModels renderF =
+--     H.div [] <| List.map renderF layerModels
